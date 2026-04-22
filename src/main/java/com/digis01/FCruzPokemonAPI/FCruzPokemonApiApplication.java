5efa566a0faad8275c44368bf5bf80451bacee27
FernandoCruzVelazquez <fernandocruzv2003@outlook.com>
2026-04-22 17:20:20 -0600

    Creacion del repo

---
+++ src/main/java/com/digis01/FCruzPokemonAPI/FCruzPokemonApiApplication.java
@@ -0,0 +1,13 @@
+package com.digis01.FCruzPokemonAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FCruzPokemonApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FCruzPokemonApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/digis01/FCruzPokemonAPI/FCruzPokemonApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digis01.FCruzPokemonAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FCruzPokemonApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
